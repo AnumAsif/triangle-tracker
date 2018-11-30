095c2c882a6b1381b6d3168378eab80a00c1d68c
Anum Asif <anum@cockar.com>
2018-11-30 14:40:07 +0300

    Call the other created funtions in eveluateSides() function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,32 @@
+function evaluateSides() {
+  document.getElementById('answerID').innerHTML = "";
+  document.getElementById("triangleTypeID").innerHTML = "";
+  var lengths = prompt("Enter lengths of a triangle to check it's type, seperated by a comma");
+
+  var sides = lengths.split(",");
+  if (sides.length == 0) {
+    alert("Enter the sides please");
+  } else if (sides.length != 3) {
+    alert("Can't find the answer. Try again");
+  } else if (isNaN(sides[0]) || isNaN(sides[1]) || isNaN(sides[2])) {
+    alert("Works only on numbers");
+  } else {
+    var a = parseInt(sides[0]);
+    var b = parseInt(sides[1]);
+    var c = parseInt(sides[2]);
+    if (isTriangle(a, b, c)) {
+      if (isEquilateral(a, b, c)) {
+        document.getElementById("triangleTypeID").innerHTML = "It's an EQUILATERAL TRIANGLE!!";
+      } else if (isIsosceles(a, b, c)) {
+        document.getElementById("triangleTypeID").innerHTML = "It's an ISOSCELES TRIANGLE!!";
+      } else {
+        document.getElementById("triangleTypeID").innerHTML = "It's a SCALENE TRIANGLE!!";
+      }
+    } else {
+      document.getElementById('answerID').innerHTML = "It is not a triangle!!";
+    }
+  }
+} /*-----------------------------------------------------------------------------------------------------------*/
 function isTriangle(a, b, c) {
   if ((a + b > c) && (b + c > a) && (c + a > b)) {
     return true;
@@ -14,13 +43,6 @@ function isEquilateral(a, b, c) {
 }
 function isIsosceles(a, b, c) {
   if (a == b || b == c || c == a) {
-    return true;
-  } else {
-    return false;
-  }
-}
-function isScalene(a, b, c) {
-  if (a != b && b != c && c != a) {
     return true;
   } else {
     return false;
